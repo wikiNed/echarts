1326c024b163d9da2dcf19e010d123cf74481319
wikiNed <1007421637@qq.com>
2019-04-18 22:27:27 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+export const mCo=({
+    title:{
+        text:'销量'
+    },
+    xAxis:{
+        data:['羊毛衫','羽绒服','风衣','大衣','帽子']
+    },
+    yAxis:{},
+    series:[{
+        name:'销量',
+        type:'bar',
+        data: [20,9,12,15,51]
+    }]
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
